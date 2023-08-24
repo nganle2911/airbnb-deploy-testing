513500d8495a3be4cc26d3f712a1ba47e6dd0a2f
Ngan LE <ngan.ltkn2911@gmail.com>
2023-08-25 00:05:35 +0200

    fixed .env

--- dist/main.js
+++ dist/main.js
@@ -11,7 +11,7 @@ async function bootstrap() {
     const config = new swagger_1.DocumentBuilder().setTitle("Airbnb_Node32").setVersion('1.0').addBearerAuth().build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup("/swagger", app, document);
-    await app.listen(3000);
+    await app.listen(8080);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
